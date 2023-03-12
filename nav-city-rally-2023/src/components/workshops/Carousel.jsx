f4c41d003926365fcb6c041582a0412734a94415
Nicholas Kozlarek <nick@Nicholass-MacBook-Pro.local>
2023-03-12 12:47:05 -0600

    Change the name of one of the workshops

--- nav-city-rally-2023/src/components/workshops/Carousel.jsx
+++ nav-city-rally-2023/src/components/workshops/Carousel.jsx
@@ -131,7 +131,7 @@ export default function Carousel () {
     },
     {
       url: 'https://images.unsplash.com/photo-1547958481-9753b5467c80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      name: 'Witnessing 101 - Sharing Christ in a Polarized Culture',
+      name: 'Witnessing 101 - Sharing Christ and Starting a Bible Study at Work',
       leader: '',
     },
     {
